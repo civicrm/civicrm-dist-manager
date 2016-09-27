4f263ed7d10c46b5759d4a05f53285dc14de4840
Tim Otten <totten@civicrm.org>
2016-09-27 16:38:45 -0400

    CheckControllerTest - Add test case

--- src/CiviUpgradeManagerBundle/Tests/Controller/CheckControllerTest.php
+++ src/CiviUpgradeManagerBundle/Tests/Controller/CheckControllerTest.php
@@ -4,13 +4,31 @@ namespace CiviUpgradeManagerBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class CheckControllerTest extends WebTestCase
-{
-    public function testCheck()
-    {
-        $client = static::createClient();
+class CheckControllerTest extends WebTestCase {
+  public function testCheck() {
+    $client = static::createClient();
 
-        $crawler = $client->request('GET', '/check');
-    }
+    $client->request('GET', '/check', array(
+      'stability' => 'rc',
+    ));
+    $response = $client->getResponse();
+    $this->assertEquals('application/json',
+      $response->headers->get('Content-type'));
+    $data = json_decode($response->getContent(), 1);
+
+    $this->assertRegex(';^[a-zA-Z0-9\-\.\+_]+$;', $data['rev']);
+    $this->assertRegex(';^https://dist.civicrm.org/by-hash/.*/civicrm-[\d\.]+-drupal(\-\d+).tar.gz;',
+      $data['tar']['Drupal']);
+    $this->assertRegex(';^https://dist.civicrm.org/by-hash/.*/civicrm-[\d\.]+-drupal6(\-\d+).tar.gz;',
+      $data['tar']['Drupal6']);
+    $this->assertRegex(';^https://dist.civicrm.org/by-hash/.*/civicrm-[\d\.]+-wordpress(\-\d+).zip;',
+      $data['tar']['WordPress']);
+  }
+
+
+  public function assertRegex($pattern, $data) {
+    $this->assertTrue((bool) preg_match($pattern, $data),
+      "Assert that pattern ($pattern) matches data ($data)");
+  }
 
 }
