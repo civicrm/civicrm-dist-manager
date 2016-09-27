ba4bb3fcf9740287591fa4d99c66b8ad7d108101
Tim Otten <totten@civicrm.org>
2016-09-27 15:26:41 -0400

    CheckController - Dummy data

--- src/CiviUpgradeManagerBundle/Controller/CheckController.php
+++ src/CiviUpgradeManagerBundle/Controller/CheckController.php
@@ -3,14 +3,35 @@
 namespace CiviUpgradeManagerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
-class CheckController extends Controller
-{
-    public function checkAction()
-    {
-        return $this->render('CiviUpgradeManagerBundle:Check:check.html.php', array(
-            // ...
-        ));
-    }
+class CheckController extends Controller {
+
+  public function checkAction() {
+    $data = array(
+      'rev' => 'c6ef392f2d68e4bc940d30e10c0e26b6-0003',
+      'tar' => array(
+        'Backdrop' => 'https://dist.civicrm.org/by-hash/c6ef392f2d68e4bc940d30e10c0e26b6-0003/civicrm-4.7.12-backdrop-unstable-20160925.tar.gz',
+        'Drupal' => 'https://dist.civicrm.org/by-hash/c6ef392f2d68e4bc940d30e10c0e26b6-0003/civicrm-4.7.12-drupal-20160925.tar.gz',
+        'Drupal6' => 'https://dist.civicrm.org/by-hash/c6ef392f2d68e4bc940d30e10c0e26b6-0003/civicrm-4.7.12-drupal6-20160925.tar.gz',
+        'Joomla' => 'https://dist.civicrm.org/by-hash/c6ef392f2d68e4bc940d30e10c0e26b6-0003/civicrm-4.7.12-joomla-20160925.zip',
+        // 'Joomla-Alt' => 'https://dist.civicrm.org/by-hash/c6ef392f2d68e4bc940d30e10c0e26b6-0003/civicrm-4.7.12-joomla-alt-20160925.zip',
+        'L10n' => 'https://dist.civicrm.org/by-hash/c6ef392f2d68e4bc940d30e10c0e26b6-0003/civicrm-4.7.12-l10n-20160925.tar.gz',
+        'WordPress' => 'https://dist.civicrm.org/by-hash/c6ef392f2d68e4bc940d30e10c0e26b6-0003/civicrm-4.7.12-wordpress-20160925.zip',
+      ),
+      'git' => array(
+        'civicrm-core' => 'FIXME',
+        'civicrm-joomla' => 'FIXME',
+        'civicrm-backdrop' => 'FIXME',
+        'civicrm-packages' => 'FIXME',
+        'civicrm-drupal' => 'FIXME',
+        'civicrm-wordpress' => 'FIXME',
+      ),
+    );
+
+    return new Response(json_encode($data), 200, array(
+      'Content-type' => 'application/json',
+    ));
+  }
 
 }
