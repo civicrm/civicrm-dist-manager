d4c07c3964275c729b32a47e23843636eb2dfdc3
Tim Otten <totten@civicrm.org>
2016-09-27 19:42:46 -0400

    CiviUpgradeManagerBundle - Implement lookup for stable releases

--- src/CiviUpgradeManagerBundle/Controller/CheckController.php
+++ src/CiviUpgradeManagerBundle/Controller/CheckController.php
@@ -2,13 +2,92 @@
 
 namespace CiviUpgradeManagerBundle\Controller;
 
+use Doctrine\Common\Cache\Cache;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckController extends Controller {
 
-  public function checkAction() {
-    $data = array(
+  const STABLE_DOWNLOAD_URL = 'https://download.civicrm.org';
+  const VERSIONS_URL = 'http://latest.civicrm.org/versions.json';
+  const CACHE_TTL = 120;
+
+  public function checkAction(Request $request) {
+    switch ($request->get('stability')) {
+      case 'rc':
+        return $this->createJson($this->getLatestRc(), 200);
+
+      case 'stable':
+        return $this->createJson($this->getLatestStable(), 200);
+
+      case 'nightly':
+        return $this->createJson($this->getLatestRc(), 200);
+
+      default:
+        return $this->createJson(array(
+          'rev' => NULL,
+          'message' => 'Missing required argument: stability=(nightly|rc|stable)'
+        ), 404);
+    }
+  }
+
+  /**
+   * @return Cache
+   */
+  protected function getCache() {
+    return $this->container->get('civi_upgrade_manager.dist_cache');
+  }
+
+  /**
+   * @return array
+   */
+  protected function getLatestStable() {
+    $versions = $this->getVersions();
+    $rev = '0';
+    foreach ($versions as $major => $majorSpec) {
+      foreach ($majorSpec['releases'] as $release) {
+        if (version_compare($release['version'], $rev, '>')) {
+          $rev = $release['version'];
+        }
+      }
+    }
+
+    return array(
+      'rev' => $rev,
+      'tar' => array(
+        'Backdrop' => sprintf('%s/civicrm-%s-backdrop-unstable.tar.gz',
+          self::STABLE_DOWNLOAD_URL, $rev),
+        'Drupal' => sprintf('%s/civicrm-%s-drupal.tar.gz',
+          self::STABLE_DOWNLOAD_URL, $rev),
+        'Drupal6' => sprintf('%s/civicrm-%s-drupal6.tar.gz',
+          self::STABLE_DOWNLOAD_URL, $rev),
+        'Joomla' => sprintf('%s/civicrm-%s-joomla.zip',
+          self::STABLE_DOWNLOAD_URL, $rev),
+        // 'Joomla-Alt' => 'https://download.civicrm.org/civicrm-4.7.12-joomla-alt.zip',
+        'L10n' => sprintf('%s/civicrm-%s-l10n.tar.gz',
+          self::STABLE_DOWNLOAD_URL, $rev),
+        'WordPress' => sprintf('%s/civicrm-%s-wordpress.zip',
+          self::STABLE_DOWNLOAD_URL, $rev),
+      ),
+      'git' => array(
+        'civicrm-core' => $rev,
+        'civicrm-joomla' => $rev,
+        'civicrm-backdrop@1.x' => "1.x-$rev",
+        'civicrm-packages' => $rev,
+        'civicrm-drupal@6.x' => "6.x-$rev",
+        'civicrm-drupal@7.x' => "7.x-$rev",
+        'civicrm-drupal@8.x' => "8.x-$rev",
+        'civicrm-wordpress' => $rev,
+      ),
+    );
+  }
+
+  /**
+   * @return array
+   */
+  protected function getLatestRc() {
+    return $data = array(
       'rev' => 'c6ef392f2d68e4bc940d30e10c0e26b6-0003',
       'tar' => array(
         'Backdrop' => 'https://dist.civicrm.org/by-hash/c6ef392f2d68e4bc940d30e10c0e26b6-0003/civicrm-4.7.12-backdrop-unstable-20160925.tar.gz',
@@ -28,10 +107,36 @@ class CheckController extends Controller {
         'civicrm-wordpress' => 'FIXME',
       ),
     );
+  }
 
-    return new Response(json_encode($data), 200, array(
+  /**
+   * @param $data
+   * @param $status
+   * @return \Symfony\Component\HttpFoundation\Response
+   */
+  protected function createJson($data, $status) {
+    return new Response(json_encode($data), $status, array(
       'Content-type' => 'application/json',
     ));
+  }
+
+  /**
+   * @return array
+   *   Ex: $result['4.7']['releases'][0]['version'] == '4.7.alpha1';
+   */
+  protected function getVersions() {
+    /** @var Cache $cache */
+    $cache = $this->getCache();
+    if (!$cache->contains('versions.json')) {
+      $data = file_get_contents(self::VERSIONS_URL);
+      if ($data) {
+        $cache->save('versions.json', $data);
+      }
+      else {
+        throw new \RuntimeException('Failed to fetch list of available versions');
+      }
+    }
+    return json_decode($cache->fetch('versions.json'), 1);
   }
 
 }
--- src/CiviUpgradeManagerBundle/Tests/Controller/CheckControllerTest.php
+++ src/CiviUpgradeManagerBundle/Tests/Controller/CheckControllerTest.php
@@ -5,7 +5,8 @@ namespace CiviUpgradeManagerBundle\Tests\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CheckControllerTest extends WebTestCase {
-  public function testCheck() {
+
+  public function testCheckRc() {
     $client = static::createClient();
 
     $client->request('GET', '/check', array(
@@ -23,6 +24,58 @@ class CheckControllerTest extends WebTestCase {
       $data['tar']['Drupal6']);
     $this->assertRegex(';^https://dist.civicrm.org/by-hash/.*/civicrm-[\d\.]+-wordpress(\-\d+).zip;',
       $data['tar']['WordPress']);
+  }
+
+  public function testCheckNightly() {
+    $client = static::createClient();
+
+    $client->request('GET', '/check', array(
+      'stability' => 'nightly',
+    ));
+    $response = $client->getResponse();
+    $this->assertEquals('application/json',
+      $response->headers->get('Content-type'));
+    $data = json_decode($response->getContent(), 1);
+
+    $this->assertRegex(';^[a-zA-Z0-9\-\.\+_]+$;', $data['rev']);
+    $this->assertRegex(';^https://dist.civicrm.org/by-hash/.*/civicrm-[\d\.]+-drupal(\-\d+).tar.gz;',
+      $data['tar']['Drupal']);
+    $this->assertRegex(';^https://dist.civicrm.org/by-hash/.*/civicrm-[\d\.]+-drupal6(\-\d+).tar.gz;',
+      $data['tar']['Drupal6']);
+    $this->assertRegex(';^https://dist.civicrm.org/by-hash/.*/civicrm-[\d\.]+-wordpress(\-\d+).zip;',
+      $data['tar']['WordPress']);
+  }
+
+  public function testCheckStable() {
+    $client = static::createClient();
+
+    $client->request('GET', '/check', array(
+      'stability' => 'stable',
+    ));
+    $response = $client->getResponse();
+    $this->assertEquals('application/json',
+      $response->headers->get('Content-type'));
+    $data = json_decode($response->getContent(), 1);
+
+    $this->assertRegex(';^[a-zA-Z0-9\-\.\+_]+$;', $data['rev']);
+    $this->assertRegex(';^https://download.civicrm.org/civicrm-[\d\.]+-drupal.tar.gz;',
+      $data['tar']['Drupal']);
+    $this->assertRegex(';^https://download.civicrm.org/civicrm-[\d\.]+-drupal6.tar.gz;',
+      $data['tar']['Drupal6']);
+    $this->assertRegex(';^https://download.civicrm.org/civicrm-[\d\.]+-wordpress.zip;',
+      $data['tar']['WordPress']);
+  }
+
+  public function testCheckUnknown() {
+    $client = static::createClient();
+
+    $client->request('GET', '/check');
+    $response = $client->getResponse();
+    $this->assertEquals(404, $response->getStatusCode());
+    $this->assertEquals('application/json',
+      $response->headers->get('Content-type'));
+    $data = json_decode($response->getContent(), 1);
+    $this->assertTrue(!empty($data['message']));
   }
 
 
