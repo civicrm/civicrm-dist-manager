3fadc0aebf44607c433cb58f6e4332e152d00502
Tim Otten <totten@civicrm.org>
2016-10-01 11:03:59 +0200

    Implement `/report` with tests.

--- src/CiviUpgradeManagerBundle/Controller/UpgradeReportController.php
+++ src/CiviUpgradeManagerBundle/Controller/UpgradeReportController.php
@@ -7,25 +7,105 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use CiviUpgradeManagerBundle\Entity\UpgradeReport;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * UpgradeReport controller.
  *
- * @Route("/UpgradeReport")
+ * @Route("/")
  */
 class UpgradeReportController extends Controller {
 
   /**
    * Lists all UpgradeReport entities.
    *
-   * @Route("/", name="UpgradeReport_index")
+   * @Route("/report", name="UpgradeReport_submit")
+   * @Method("POST")
+   */
+  public function submitAction(Request $request) {
+    $em = $this->getDoctrine()->getManager();
+
+    foreach (array('siteId', 'name') as $field) {
+      if (!$request->get($field)) {
+        return $this->createJson(array('message' => "Missing required argument: $field"), 400); // FIXME: Status code
+      }
+    }
+
+    /** @var UpgradeReport $upgradeReport */
+    $upgradeReport = $em->getRepository('CiviUpgradeManagerBundle:UpgradeReport')
+      ->find($request->get('name'));
+
+    if ($upgradeReport && $upgradeReport->getSiteId() !== $request->get('siteId')) {
+      return $this->createJson(array('message' => "Report already exists. Claimed by different siteId."), 400); // FIXME: Status code
+    }
+
+    if (!$upgradeReport) {
+      $upgradeReport = new UpgradeReport();
+      $upgradeReport->setName($request->get('name'));
+      $upgradeReport->setSiteId($request->get('siteId'));
+
+      if (!$request->get('revision')) {
+        return $this->createJson(array('message' => "Missing required argument: revision"), 400); // FIXME: Status code
+      }
+      $upgradeReport->setRevision($request->get('revision'));
+      $em->persist($upgradeReport);
+    }
+
+    $fields = array(
+      'reporter'=> 'basic',
+      'downloadUrl'=> 'basic',
+      'startReport'=> 'basic',
+      'upgradeReport'=> 'basic',
+      'finishReport'=> 'basic',
+      'problem'=> 'basic',
+      'started' => 'ts',
+      'downloaded' => 'ts',
+      'extracted' => 'ts',
+      'upgraded' => 'ts',
+      'finished' => 'ts',
+      'failed' => 'ts',
+    );
+    foreach ($fields as $field => $type) {
+      if (!$request->get($field)) {
+        continue;
+      }
+
+      if ($upgradeReport->get($field)) {
+        return $this->createJson(array('message' => "Field $field has already been set"), 400); // FIXME: Status code
+      }
+      else {
+        switch ($type) {
+          case 'basic':
+            $upgradeReport->set($field, $request->get($field));
+            break;
+          case 'ts':
+            $upgradeReport->set($field, new \DateTime(date('r', $request->get($field))));
+            break;
+          default:
+            throw new \RuntimeException("Unrecognized field type: $type");
+        }
+      }
+    }
+
+    $upgradeReport->updateComputedFields();
+    $em->flush();
+
+    return $this->createJson(array('message' => 'Saved'), 200);
+  }
+
+
+  /**
+   * Lists all UpgradeReport entities.
+   *
+   * @Route("/report/review", name="UpgradeReport_index")
    * @Method("GET")
    */
   public function indexAction() {
     $em = $this->getDoctrine()->getManager();
 
     $upgradeReports = $em->getRepository('CiviUpgradeManagerBundle:UpgradeReport')
-      ->findAll();
+        ->findBy([], ['started' => 'DESC']);
 
     return $this->render('upgradereport/index.html.twig', array(
       'upgradeReports' => $upgradeReports,
@@ -35,13 +115,26 @@ class UpgradeReportController extends Controller {
   /**
    * Finds and displays a UpgradeReport entity.
    *
-   * @Route("/{id}", name="UpgradeReport_show")
+   * @Route("/report/review/{id}", name="UpgradeReport_show")
    * @Method("GET")
    */
-  public function showAction(UpgradeReport $upgradeReport) {
+  public function showAction(
+    UpgradeReport $upgradeReport
+  ) {
 
     return $this->render('upgradereport/show.html.twig', array(
       'upgradeReport' => $upgradeReport,
+    ));
+  }
+
+  /**
+   * @param $data
+   * @param $status
+   * @return \Symfony\Component\HttpFoundation\Response
+   */
+  protected function createJson($data, $status) {
+    return new Response(json_encode($data), $status, array(
+      'Content-type' => 'application/json',
     ));
   }
 
--- src/CiviUpgradeManagerBundle/Tests/Controller/UpgradeReportControllerTest.php
+++ src/CiviUpgradeManagerBundle/Tests/Controller/UpgradeReportControllerTest.php
@@ -2,24 +2,158 @@
 
 namespace CiviUpgradeManagerBundle\Tests\Controller;
 
+use CiviUpgradeManagerBundle\Entity\UpgradeReport;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class UpgradeReportControllerTest extends WebTestCase {
-  /*
-  public function testCompleteScenario()
-  {
-      // Create a new client to browse the application
-      $client = static::createClient();
 
-      // Go to the list view
-      $crawler = $client->request('GET', '/UpgradeReport/');
-      $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /UpgradeReport/");
-
-      // Go to the show view
-      $crawler = $client->click($crawler->selectLink('show')->link());
-      $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code");
+  public function setUp() {
+    parent::setUp();
+    $em = $this->createEntityManager();
+    $em->getConnection()->executeQuery('DELETE FROM UpgradeReport WHERE siteId LIKE "UpgradeReport%"');
   }
 
-  */
+  public function testRequired() {
+    // Create a new client to browse the application
+    $client = static::createClient();
+    $siteId = 'UpgradeReportControllerTest';
+    $name = md5(rand() . rand() . rand() . uniqid() . time());
+
+    $client->request('POST', '/report', array(
+      'name' => $name,
+    ));
+    $json = $this->checkJsonResponse($client->getResponse(), 400);
+    $this->assertEquals('Missing required argument: siteId', $json['message']);
+
+    $client->request('POST', '/report', array(
+      'siteId' => $siteId,
+    ));
+    $json = $this->checkJsonResponse($client->getResponse(), 400);
+    $this->assertEquals('Missing required argument: name', $json['message']);
+  }
+
+  public function testCompleteScenario() {
+    // Create a new client to browse the application
+    $client = static::createClient();
+    $siteId = 'UpgradeReportControllerTest';
+    $name = md5(rand() . rand() . rand() . uniqid() . time());
+
+    $client->request('POST', '/report', array(
+      'siteId' => $siteId,
+      'name' => $name,
+      'revision' => '1.0.2',
+      'started' => strtotime('2001-02-03 12:00'),
+    ));
+    $json = $this->checkJsonResponse($client->getResponse(), 200);
+    $this->assertEquals('Saved', $json['message']);
+
+    // Go to the list view
+    $crawler = $client->request('GET', '/report/review');
+    $this->assertEquals(200, $client->getResponse()->getStatusCode(),
+      "Unexpected HTTP status code for GET /UpgradeReport/");
+    $this->assertContains($name, $crawler->text());
+    $this->assertNotContains($siteId, $crawler->text());
+
+    // Go to the show view
+    $crawler = $client->click($crawler->selectLink('show')->link());
+    $this->assertEquals(200, $client->getResponse()->getStatusCode(),
+      "Unexpected HTTP status code");
+    $this->assertContains($name, $crawler->text());
+    $this->assertNotContains($siteId, $crawler->text());
+    $this->assertContains('2001-02-03', $crawler->text());
+  }
+
+  public function testUpdateProperty() {
+    // Create a new client to browse the application
+    $client = static::createClient();
+    $siteId = 'UpgradeReportControllerTest';
+    $name = md5(rand() . rand() . rand() . uniqid() . time());
+
+    $client->request('POST', '/report', array(
+      'siteId' => $siteId,
+      'name' => $name,
+      'revision' => '1.0.2',
+      'started' => strtotime('2001-03-03 12:00'),
+    ));
+    $json = $this->checkJsonResponse($client->getResponse(), 200);
+    $this->assertEquals('Saved', $json['message']);
+
+    /** @var UpgradeReport $upgradeReport */
+    $em = $this->createEntityManager();
+    $upgradeReport = $em->find(UpgradeReport::class, $name);
+    $this->assertEquals(strtotime('2001-03-03 12:00'), $upgradeReport->getStarted()->getTimestamp());
+    $this->assertEquals(NULL, $upgradeReport->getFinished());
+    $this->assertEquals('downloading', $upgradeReport->getStage());
+
+    $client->request('POST', '/report', array(
+      'siteId' => $siteId,
+      'name' => $name,
+      'finished' => strtotime('2001-03-05 12:00'),
+    ));
+    $json = $this->checkJsonResponse($client->getResponse(), 200);
+    $this->assertEquals('Saved', $json['message']);
+
+    $em->refresh($upgradeReport);
+    $this->assertEquals(strtotime('2001-03-03 12:00'), $upgradeReport->getStarted()->getTimestamp());
+    $this->assertEquals(strtotime('2001-03-05 12:00'), $upgradeReport->getFinished()->getTimestamp());
+    $this->assertEquals('finished', $upgradeReport->getStage());
+  }
+
+
+  public function testWrongSiteId() {
+    // Create a new client to browse the application
+    $client = static::createClient();
+    $siteId = 'UpgradeReportControllerTest';
+    $name = md5(rand() . rand() . rand() . uniqid() . time());
+
+    $client->request('POST', '/report', array(
+      'siteId' => $siteId,
+      'name' => $name,
+      'revision' => '1.0.2',
+      'started' => strtotime('2001-03-03 12:00'),
+    ));
+    $json = $this->checkJsonResponse($client->getResponse(), 200);
+    $this->assertEquals('Saved', $json['message']);
+
+    /** @var UpgradeReport $upgradeReport */
+    $em = $this->createEntityManager();
+    $upgradeReport = $em->find(UpgradeReport::class, $name);
+    $this->assertEquals(strtotime('2001-03-03 12:00'), $upgradeReport->getStarted()->getTimestamp());
+    $this->assertEquals(NULL, $upgradeReport->getFinished());
+
+    $client->request('POST', '/report', array(
+      'siteId' => 'wrongSiteId',
+      'name' => $name,
+      'finished' => strtotime('2001-03-05 12:00'),
+    ));
+    $json = $this->checkJsonResponse($client->getResponse(), 400);
+    $this->assertEquals('Report already exists. Claimed by different siteId.',
+      $json['message']);
+
+    $em->refresh($upgradeReport);
+    $this->assertEquals(strtotime('2001-03-03 12:00'), $upgradeReport->getStarted()->getTimestamp());
+    $this->assertEquals(NULL, $upgradeReport->getFinished());
+  }
+
+  protected function checkJsonResponse(Response $response, $expectCode) {
+    $this->assertEquals('application/json',
+      $response->headers->get('Content-type'));
+    $this->assertEquals($expectCode, $response->getStatusCode(),
+      "Unexpected HTTP status code. Content:" . $response->getContent());
+    $data = json_decode($response->getContent(), 1);
+    return $data;
+  }
+
+  /**
+   * @return \Doctrine\ORM\EntityManager
+   */
+  protected function createEntityManager() {
+    $client = static::createClient();
+    /** @var EntityManager $em */
+    $em = $client->getKernel()->getContainer()->get('doctrine')->getManager();
+    return $em;
+  }
 
 }
