e11123756a6aa557b6f80ec8ace23f5669f67820
Tim Otten <totten@civicrm.org>
2017-04-20 21:34:20 -0700

    GCloudFactory - Add factory for StorageClient and Bucket

---
+++ src/CiviUpgradeManagerBundle/GCloudFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace CiviUpgradeManagerBundle;
+
+use Google\Cloud\Storage\StorageClient;
+
+class GCloudFactory {
+
+  /**
+   * @param $credFile
+   * @param $projectId
+   * @return \Google\Cloud\Storage\StorageClient
+   */
+  public static function createStorageClient($credFile, $projectId) {
+    if (file_exists($credFile)) {
+      putenv("GOOGLE_APPLICATION_CREDENTIALS=$credFile");
+    }
+    $storage = new StorageClient([
+      'projectId' => $projectId,
+    ]);
+    return $storage;
+  }
+
+  /**
+   * @param \Google\Cloud\Storage\StorageClient $client
+   * @param string $bucketName
+   * @return \Google\Cloud\Storage\Bucket
+   */
+  public static function createBucket(StorageClient $client, $bucketName) {
+    return $client->bucket($bucketName);
+  }
+
+}
