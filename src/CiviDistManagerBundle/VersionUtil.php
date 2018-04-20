b882c090085928f7ba6479bc8e4b195bd732bc81
Tim Otten <totten@civicrm.org>
2018-04-19 18:13:29 -0700

    VersionUtil - Add more helpers

--- src/CiviDistManagerBundle/VersionUtil.php
+++ src/CiviDistManagerBundle/VersionUtil.php
@@ -3,6 +3,49 @@ namespace CiviDistManagerBundle;
 
 class VersionUtil {
 
+  /**
+   * @param string $ver
+   *   Ex: '5.1.2'
+   * @return string
+   *   Ex: '5'
+   */
+  public static function getMajor($ver) {
+    $parts = preg_split('/[\.\+]/', $ver);
+    return $parts[0];
+  }
+
+  /**
+   * @param string $ver
+   *   Ex: '5.1.2'
+   * @return string
+   *   Ex: '5.1'
+   */
+  public static function getMinor($ver) {
+    $parts = preg_split('/[\.\+]/', $ver);
+    return $parts[0] . '.' . $parts[1];
+  }
+
+  /**
+   * @param string $ver
+   *   Ex: '5.1.2'
+   * @return string
+   *   Ex: '5.1.2'
+   */
+  public static function getPatch($ver) {
+    $parts = preg_split('/[\.\+]/', $ver);
+    return $parts[0] . '.' . $parts[1] . '.' . $parts[2];
+  }
+
+  /**
+   * @param string $ver
+   *   Ex: '4.7.10', '5.1.beta1', '<script>'.
+   * @return bool
+   *   TRUE if the string is a well-formed version number.
+   */
+  public static function isWellFormed($ver) {
+    return (bool) preg_match(';^[0-9]+\.[0-9a-z_\-\.]+$;', $ver);
+  }
+
   public static function max($versions) {
     $extreme = NULL;
     foreach ($versions as $v) {
