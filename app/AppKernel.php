7aa1645e60c0fa96a27827fb3e8c454e5edabc6e
Tim Otten <totten@civicrm.org>
2023-08-30 01:10:51 -0700

    Remove sensio/framework-extra-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -12,7 +12,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new CiviDistManagerBundle\CiviDistManagerBundle(),
         ];
