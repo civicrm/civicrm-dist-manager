ffa667701958e1d203ac3be14b1c01b9bc0c30a6
Tim Otten <totten@civicrm.org>
2023-08-30 02:54:50 -0700

    Update composer

--- shell.nix
+++ shell.nix
@@ -13,12 +13,15 @@ in
       buildkit.pkgs.php74
       buildkit.pins.v2305.nginx
       buildkit.pkgs.loco
-      (buildkit.funcs.fetchPhar {
-        name = "composer";
-        url = "https://github.com/composer/composer/releases/download/2.2.21/composer.phar";
-        sha256 = "5211584ad39af26704da9f6209bc5d8104a2d576e80ce9c7ed8368ddd779d0af";
-      })
       buildkit.pkgs.phpunit8
+
+      buildkit.pkgs.composer
+      ## Or if you need a specific revision:
+      # (buildkit.funcs.fetchPhar {
+      #   name = "composer";
+      #   url = "https://github.com/composer/composer/releases/download/2.2.21/composer.phar";
+      #   sha256 = "5211584ad39af26704da9f6209bc5d8104a2d576e80ce9c7ed8368ddd779d0af";
+      # })
 
       pkgs.bash-completion
     ];
