785fdd5a55e635886123c671444b22dd4aabff4f
Tim Otten <totten@civicrm.org>
2023-08-30 19:20:43 -0700

    Fix download of 'civicrm-*-wporg.zip'

--- src/CiviDistManagerBundle/LegacyRedirect.php
+++ src/CiviDistManagerBundle/LegacyRedirect.php
@@ -7,7 +7,7 @@ class LegacyRedirect {
     // ex: /civicrm-4.7.10-drupal.tar.gz
     // ex: https://storage.googleapis.com/civicrm/civicrm-stable/4.7.12/civicrm-4.7.12-backdrop-unstable.tar.gz
     $base = "https://storage.googleapis.com/civicrm";
-    if (preg_match(";^/civicrm-([0-9a-z\.]+)(-drupal|-joomla|-wordpress|-l10n|-back|-standalone|-starter|\.MD5|\.SHA256|\.json);", $path, $matches)) {
+    if (preg_match(";^/civicrm-([0-9a-z\.]+)(-drupal|-joomla|-wordpress|-wporg|-l10n|-back|-standalone|-starter|\.MD5|\.SHA256|\.json);", $path, $matches)) {
       $version = $matches[1];
       if (preg_match(';(alpha|beta);', $version)) {
         return "$base/civicrm-testing/$version$path";
