8a0480e3127f84f1c77dda6af5383c43b03055cd
Tim Otten <totten@civicrm.org>
2025-06-26 16:41:33 -0700

    Autobuild UI - Start switching UI to resemble releases

--- src/CiviDistManagerBundle/Controller/BrowseController.php
+++ src/CiviDistManagerBundle/Controller/BrowseController.php
@@ -3,9 +3,8 @@
 namespace CiviDistManagerBundle\Controller;
 
 use CiviDistManagerBundle\BuildRepository;
+use CiviDistManagerBundle\CacheTrait;
 use CiviDistManagerBundle\GitBrowsers;
-use CiviDistManagerBundle\VersionUtil;
-use Doctrine\Common\Cache\Cache;
 use Google\Cloud\Core\Timestamp;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,81 +12,40 @@ use Symfony\Component\HttpFoundation\Response;
 
 class BrowseController extends Controller {
 
-  const CACHE_TTL = 120;
+  use CacheTrait;
+
+  public function __construct() {
+    $this->standardTtl = 120;
+    $this->errorTtl = 30;
+  }
 
   /**
    * Display a list of builds and files. (HTML)
    *
    * @param \Symfony\Component\HttpFoundation\Request $request
+   * @param string $branch
    * @return \Symfony\Component\HttpFoundation\Response
    */
-  public function browseAction(Request $request) {
-    /** @var BuildRepository $buildRepo */
-    $buildRepo = $this->container->get('build_repository');
-
-    $files = $buildRepo->getFilesByWildcard($request->get('branch') . '/');
-    $byTimestamp = array();
-
-    if (!empty($files)) {
-      $now = time();
-      $basenames = [
-        'civicrm-X.Y.Z-backdrop-LATEST.tar.gz',
-        'civicrm-X.Y.Z-drupal-LATEST.tar.gz',
-        'civicrm-X.Y.Z-drupal6-LATEST.tar.gz',
-        'civicrm-X.Y.Z-joomla-LATEST.zip',
-        'civicrm-X.Y.Z-joomla-alt-LATEST.zip',
-        'civicrm-X.Y.Z-l10n-LATEST.tar.gz',
-        'civicrm-X.Y.Z-standalone-LATEST.tgz',
-        'civicrm-X.Y.Z-starterkit-LATEST.tgz',
-        'civicrm-X.Y.Z-wordpress-LATEST.zip',
-        'civicrm-X.Y.Z-wporg-LATEST.zip',
-      ];
-
-      foreach ($basenames as $basename) {
-        $files[] = [
-          'file' => 'LATEST/' . $basename,
-            'basename' => $basename,
-            'branch' => $request->get('branch'),
-            'version' => 'X.Y.Z',
-            // 'url' => NULL,
-            'rev' => 'X.Y.Z-LATEST',
-            // 'uf' => 'Backdrop',
-            'ts' => 'LATEST',
-            'timestamp' => $now,
-        ];
-      }
+  public function browseAction(Request $request, $branch) {
+    if (!$this->isWellFormedBranch($branch)) {
+      throw $this->createNotFoundException("Invalid branch");
     }
 
-    foreach ($files as $file) {
-      $key = $file['ts'];
-      if (!isset($byTimestamp[$key])) {
-        $byTimestamp[$key] = array(
-          'fmt' => date('Y-m-d H:i:s T', $file['timestamp']),
-          'timestamp' => $file['timestamp'],
-          'ts' => $file['ts'],
-          'files' => array(),
-        );
-      }
-      $file['_download_url'] = $this->generateUrl('download_branch_file', array(
-        'branch' => $file['branch'],
-        'ts' => $file['ts'],
-        'basename' => $file['basename'],
-      ));
-      $file['_inspect_url'] = $this->generateUrl('inspect_branch_build', array(
-        'branch' => $file['branch'],
-        'ts' => $file['ts'],
-        'basename' => $file['basename'],
-      ));
-      $byTimestamp[$key]['files'][] = $file;
-    }
-
-    uasort($byTimestamp, function($a, $b){
-      return -1 * strnatcmp($a['ts'], $b['ts']);
+    $timestamps = $this->getFolders($branch);
+    usort($timestamps, function ($a, $b) {
+      return -1 * strnatcmp($a, $b);
     });
+    array_unshift($timestamps, 'LATEST');
 
     return $this->render('CiviDistManagerBundle:Browse:browse.html.twig', array(
+      'breadcrumbs' => [
+        ['title' => 'CiviCRM Home', 'url' => 'https://civicrm.org/'],
+        ['title' => 'Download', 'url' => 'https://civicrm.org/download'],
+        ['title' => 'Autobuild', 'url' => '/latest/'],
+        ['title' => $request->get('branch')],
+      ],
       'targetBranch' => $request->get('branch'),
-      'byTimestamp' => $byTimestamp,
+      'folders' => $timestamps,
     ));
   }
 
@@ -106,27 +64,57 @@ class BrowseController extends Controller {
   }
 
   /**
-   * View the build report for the build (HTML).
+   * Get an overview about specific build
    *
-   * Ex: "GET /latest/branch/master/201801140252/inspect".
-
    * @param \Symfony\Component\HttpFoundation\Request $request
-   * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+   * @param $branch
+   * @param $ts
+   * @return \Symfony\Component\HttpFoundation\Response|null
    */
-  public function inspectAction(Request $request) {
-    /** @var BuildRepository $buildRepo */
-    $buildRepo = $this->container->get('build_repository');
+  public function viewAction(Request $request, $branch, $ts) {
+    if (!$this->isWellFormedBranch($branch)) {
+      throw $this->createNotFoundException("Invalid branch");
+    }
 
-    $file = $this->getFile($request->get('branch'), $request->get('ts'), $request->get('basename'));
+    if ($ts === 'LATEST') {
+      $title = $ts;
+      $ts = $this->getLatest($branch);
+      $releaseFiles = $this->getLatestReleaseFiles($branch);
+      $jsonFile = preg_grep(';\.json$;', array_column($this->getReleaseFiles($branch, $this->getLatest($branch)), 'basename'))[0];
+    }
+    else {
+      $title = $ts;
+      if (!$this->isWellFormedTimestamp($ts)) {
+        throw $this->createNotFoundException("Invalid timestamp");
+      }
+      $releaseFiles = $this->getReleaseFiles($branch, $ts);
+      $jsonFile = preg_grep(';\.json$;', array_column($releaseFiles, 'basename'))[0];
+    }
 
-    $jsonDef = $buildRepo->fetchJsonDef($file['url']);
-
-    return $this->render('CiviDistManagerBundle:Check:inspect.html.twig', array(
-      'file' => $file['basename'],
-      'fileUrl' => $file['url'],
-      'jsonDef' => $jsonDef,
-      'gitBrowsers' => GitBrowsers::getAll('/commits'),
-    ));
+    if (!empty($releaseFiles)) {
+      $json = $this->getReleaseJson($branch, $ts, $jsonFile);
+      return $this->render('CiviDistManagerBundle:About:about.html.twig', [
+        'breadcrumbs' => [
+          ['title' => 'CiviCRM Home', 'url' => 'https://civicrm.org/'],
+          ['title' => 'Download', 'url' => 'https://civicrm.org/download'],
+          ['title' => 'Autobuild', 'url' => '/latest/'],
+          ['title' => $branch, 'url' => '/latest/branch/' . $branch . '/'],
+          ['title' => $title],
+        ],
+        'version' => $json['version'],
+        'files' => $releaseFiles,
+        // 'notes' => $this->getReleaseNotes($version),
+        'jsonDef' => $json,
+        'gitBrowsers' => GitBrowsers::getAll(),
+      ]);
+    }
+    else {
+      $response = $this->render('CiviDistManagerBundle:About:unknown.html.twig', [
+        'version' => "$branch/$ts",
+      ]);
+      $response->setStatusCode(404);
+      return $response;
+    }
   }
 
   /**
@@ -166,6 +154,119 @@ class BrowseController extends Controller {
       throw $this->createNotFoundException();
     }
     return $file;
+  }
+
+  protected function getFolders(string $branch): array {
+    $files = static::cache("autobuild-list-$branch", function() use ($branch) {
+      /** @var \CiviDistManagerBundle\GStorageUrlFacade $gsu */
+      $gsu = $this->container->get('gsu');
+      $gsFiles = $gsu->getFiles('gs://civicrm-build/' . $branch . '/');
+      $jsonFiles = preg_grep(';\.json$;', $gsFiles);
+      asort($jsonFiles);
+      return $jsonFiles;
+    }, []);
+
+    $result = [];
+    foreach ($files as $file => $uri) {
+      if (preg_match(';-(\d+).json$;', $file, $m)) {
+        $result[] = $m[1];
+      }
+    }
+    sort($result);
+
+    return $result;
+  }
+
+  protected function getLatest(string $branch): string {
+    $all = $this->getFolders($branch);
+    sort($all);
+    return $all[0];
+  }
+
+  /**
+   * @param string $branch
+   * @param string $ts
+   *
+   * @return string[]
+   *   List of file names and their URLs.
+   *   Ex: [['basename' => 'civicrm-1.2.3-drupal.tar.gz', 'url' => 'https://....']]
+   */
+  protected function getReleaseFiles($branch, $ts) {
+    // $files = static::cache(NULL, function() use ($branch, $ts) {
+    $files = static::cache("autobuild-files-$branch-$ts", function() use ($branch, $ts) {
+      /** @var \CiviDistManagerBundle\GStorageUrlFacade $gsu */
+      $gsu = $this->container->get('gsu');
+      $gsFiles = $gsu->getFiles('gs://civicrm-build/' . $branch . '/');
+      $fmtFiles = [];
+      foreach ($gsFiles as $gsFile) {
+        if (strpos(basename($gsFile), '-' . $ts)) {
+          $fmtFiles[] = $gsFile;
+        }
+      }
+      asort($fmtFiles);
+      return $fmtFiles;
+    }, []);
+
+    $result = [];
+    foreach ($files as $file) {
+      $result[] = [
+        'basename' => basename($file),
+        'url' => basename($file),
+      ];
+    }
+    return $result;
+  }
+
+  protected function getLatestReleaseFiles($branch): array {
+    $latest = $this->getLatest($branch);
+    $files = $this->getReleaseFiles($branch, $latest);
+    foreach ($files as &$file) {
+      foreach (['basename', 'url'] as $prop) {
+        // This makes it easier to re-use the link in the future.
+        $file[$prop] = str_replace($latest, 'LATEST', $file[$prop]);
+        $file[$prop] = preg_replace('/-(\d|alpha|beta|\.)+-/', '-X.Y.Z-', $file[$prop]);
+      }
+    }
+    return $files;
+  }
+
+  /**
+   * @param string $branch
+   * @param string $ts
+   * @param string $jsonFile
+   * @return array
+   */
+  protected function getReleaseJson($branch, $ts, $jsonFile) {
+    // return static::cache(NULL, function() use ($branch, $ts, $jsonFile) {
+    return static::cache("autobuild-json-$branch-$ts-$jsonFile", function() use ($branch, $ts, $jsonFile) {
+      /** @var \Google\Cloud\Storage\Bucket $bucket */
+      $bucket = $this->container->get('gcloud_build_bucket');
+      $name = "$branch/$jsonFile";
+      $rawJson = $bucket->object($name)->downloadAsString();
+      $parsedJson = json_decode($rawJson, TRUE);
+      if (empty($rawJson) || empty($parsedJson)) {
+        throw new \Exception("JSON file ($name) had no valid JSON data");
+      }
+      return $parsedJson;
+    }, []);
+  }
+
+  /**
+   * @param $branch
+   *
+   * @return false|int
+   */
+  protected function isWellFormedBranch($branch) {
+    return preg_match(';^[-a-z0-9]+(\.[-a-z0-9]+)?$;', $branch) && !preg_match(';-(security|esr);', $branch);
+  }
+
+  /**
+   * @param $ts
+   *
+   * @return false|int
+   */
+  protected function isWellFormedTimestamp($ts) {
+    return preg_match(';^\d+$;', $ts) && mb_strlen($ts) > 10;
   }
 
 }
